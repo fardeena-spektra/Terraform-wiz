d9f2729b0d595b4a7b0157f36d5a15d21c9a2052
fardeena-spektra <fardeen.attar@spektrasystems.com>
2026-09-21 19:52:27 +0530

    Create dck.sh

---
+++ dck.sh
@@ -0,0 +1,5 @@
+docker exec -d f7456ed24788 sh -c 'cd /volumes && python3 tunserver.py > server.log 2>&1'
+docker exec -d b8d639ab0976 sh -c 'cd /volumes && python3 tunclient.py > client.log 2>&1'
+sleep 5
+docker top f7456ed24788 -eo args | grep tun
+docker top b8d639ab0976 -eo args | grep tun
